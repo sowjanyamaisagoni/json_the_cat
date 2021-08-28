1a871b6b1bb7ede6011afd6a9ee034bc3e680058
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-08-28 18:54:14 -0400

    cats

---
+++ breedFetcher.js
@@ -0,0 +1,28 @@
+const request = require('request');
+const [ breed ] = process.argv.slice(2);
+//console.log(breed);
+request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, {json: true}, (err, res, body) => {
+//console.log(body);
+
+
+if (err) {
+
+    console.log('req fail');
+    if (err.errno === -3008) {
+      console.log('ERROR IS :', err);
+    }
+
+    return;
+  }
+  if('message' in body&&body.message.includes('404')) {
+    console.log("req fail");
+    return;
+  } 
+
+  if (JSON.stringify(body) === JSON.stringify([])) {
+    console.log('Breed is not found');
+    return;
+  }
+ const breedobj = body[0];
+ console.log(breedobj.description);
+}); 
